001c583b473758fbbe32ce34e5f12d2cb090c22d
Jade-m22 <mj.micheljade@gmail.com>
2025-09-04 13:40:11 +0200

    create subpages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docs',
           position: 'left',
           label: 'Tutorial',
         },
